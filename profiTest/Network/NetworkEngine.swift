fb22664558ab7037747fb5a5cde08f8a51e47a92
v-nereutsa <45881935+v-nereutsa@users.noreply.github.com>
2021-10-23 15:22:18 +0300

    NetworkEngine changes

--- profiTest/Network/NetworkEngine.swift
+++ profiTest/Network/NetworkEngine.swift
@@ -9,13 +9,33 @@ import Foundation
 
 class NetworkEngine: NetworkClient {
     
+    static let shared: NetworkClient = NetworkEngine()
+    
     func execute(command: NetworkCommand, completion: @escaping (Result<Data, Error>) -> Void) {
-        let url = command.getQuery()!
+        var query = URLComponents()
+        query.scheme = command.scheme
+        query.host = command.host
+        query.path = command.path
+        query.queryItems = command.params.map{(key, value) in URLQueryItem(name: key, value: value)}
         
+        let url = query.url!
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-            completion(error != nil ? .failure(error!) : .success(data!))
+            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                completion(.failure(NetworkError.invalidResponseCode(responseCode: response.statusCode, errorDescription: "Invalid response code.")))
+                return
+            }
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(NetworkError.invalidData(errorDescription: "Invalid data.")))
+                return
+            }
+            completion(.success(data))
         }
         task.resume()
     }
     
 }
+    
---
+++ profiTest/Network/NetworkError.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkError.swift
+//  profiTest
+//
+//  Created by Владимир Нереуца on 23.10.2021.
+//
+
+enum NetworkError: Error {
+    case invalidResponseCode(responseCode: Int, errorDescription: String)
+    case invalidData(errorDescription: String)
+}
